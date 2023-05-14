1444d22c3510b0832686f51d73b5b4e2d7c39c09
phamanhduy1234 <116532203+phamanhduy1234@users.noreply.github.com>
2023-05-14 20:07:31 +0700

    up

---
+++ index.js
@@ -0,0 +1,51 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
+import { getDatabase, ref, set, child, get } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDHc2jfrh4b38ctfEXNT09IdDPh6Q-EsfI",
+  authDomain: "loztoan.firebaseapp.com",
+  databaseURL: "https://loztoan-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "loztoan",
+  storageBucket: "loztoan.appspot.com",
+  messagingSenderId: "573098348380",
+  appId: "1:573098348380:web:c8c7dd5a37371569b43b99"
+  
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+send.addEventListener('click',(e) =>{ 
+Sends();
+});
+read.addEventListener('click',(e) =>{ 
+Getval();
+});
+
+function Sends(){
+    Number = parseFloat(document.getElementById('Number').value);
+const db = getDatabase();
+  set(ref(db, 'Number/'), {
+    Value : Number
+  });
+}
+
+function Getval(){
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `Number` + '/Value')).then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+        document.getElementById('val').innerHTML = "Number/Value="+snapshot.val();
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+}
